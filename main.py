44ff4d2fc4a15c0ae734a156c7da08944534f2c0
Tackro <dnews@btinternet.com>
2024-12-22 16:39:38 +0000

    Print Report function, path reference in get_book_contents fixed

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 def main():
+    
 
     path = "frankenstein.txt"
     book_contents = get_book_contents(path)
 
     word_count = count_words(book_contents)
     character_appearances = count_character_appearances(book_contents)
-    print(character_appearances)
+    print_report(path, word_count, character_appearances)
     
 
-def get_book_contents(book_file):
+def get_book_contents(path):
     
-    with open("books/frankenstein.txt") as book:
+    with open(f"books/{path}") as book:
         return book.read()
 
 def count_words(book_contents):
@@ -32,5 +33,18 @@ def count_character_appearances(book_contents):
     
     return char_dict
 
-main()
+def print_report(path, word_count, character_appearances):
+    print(f"--- Begin report of {path} ---")
+    print(f"{word_count} words found in the document\n")
 
+    #Sort Dictionary by character regularity
+    sorted_dictionary = dict(sorted(character_appearances.items(), key=lambda x:x[1], reverse=True))
+
+    for char in sorted_dictionary:
+        if char.isalpha() == False: continue
+        print(f"The '{char}' character was found {sorted_dictionary[char]} times")
+
+    print("--- End report ---")
+
+#Intended to be run from root of bookbot repository
+main()
