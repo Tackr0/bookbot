5ed153abe2076cede781d9341dfe81bd6eb456e2
Tackro <dnews@btinternet.com>
2024-12-22 16:04:55 +0000

    Reworked main.py main() to act only as master function and introduced count_words() function

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 def main():
 
+    path = "frankenstein.txt"
+    book_contents = get_book_contents(path)
+
+    book_word_count = count_words(book_contents)
+    print(book_word_count)
+
+def get_book_contents(book_file):
+    
     with open("books/frankenstein.txt") as book:
-        book_contents = book.read()
-        print(book_contents)
+        return book.read()
+
+def count_words(book_contents):
+    split_words = str.split(book_contents)
+    return len(split_words)
 
 main()
 
